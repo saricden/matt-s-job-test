f73115ad1c6d5bdb5882a88e06d7d4c4d6d4be58
Kirk Morris <worker.kirk@gmail.com>
2020-11-19 18:51:36 -0800

    Adds migrations and seeder.

---
+++ database/migrations/2020_11_19_172141_create_picture_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePictureTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('album_id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('img');
+            $table->boolean('featured');
+            $table->date('op_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use DB;
+use App\Models\Photographer;
+use App\Models\Album;
+use App\Models\Picture;
+use File;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('photographers')->delete();
+        DB::table('albums')->delete();
+        DB::table('pictures')->delete();
+        $json = File::get('database/data/landscapes.json');
+        $photographerData = json_decode($json);
+
+        $photographer = Photographer::create([
+            'name'            => $photographerData->name,
+            'bio'             => $photographerData->bio,
+            'phone'           => $photographerData->phone,
+            'email'           => $photographerData->email,
+            'profile_picture' => $photographerData->profile_picture
+        ]);
+
+        $album = Album::create([
+            'owner_id' => $photographer->id
+        ]);
+
+        foreach ($photographerData->album as $newPhoto) {
+            Picture::create([
+                'album_id'    => $album->id,
+                'title'       => $newPhoto->title,
+                'description' => $newPhoto->description,
+                'img'         => $newPhoto->img,
+                'op_date'     => $newPhoto->date,
+                'featured'    => $newPhoto->featured
+            ]);
+        }
+
     }
 }
